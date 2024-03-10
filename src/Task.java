0c37a403886b7722bcdf88a7c68a7eaced30b9da
mayak123 <126418191+mayak123@users.noreply.github.com>
2024-03-10 09:19:18 +0200

    Create Task.java

---
+++ src/Task.java
@@ -0,0 +1,33 @@
+	public static void insert1(Node<Task> l, Task task1) {
+		Node<Task>p=l;
+		Node<Task>l1 = new Node<Task> (null);
+		Node<Task>p1=l1;
+		int count =0;
+		while(p!=null&&count!=1) {
+			if(p.getValue().getPriority()<=task1.getPriority()) {
+				p1.setNext(new Node <Task> (p.getValue()));	
+			}
+			else {
+				p1.setNext(new Node <Task> (task1));
+				count++;
+			}
+		}
+		if(count!=1) {
+			p1.setNext(new Node <Task> (task1));	
+		}
+		while(p1!=null) {
+			p1.setNext(new Node <Task> (p.getValue()));	
+		}
+
+	}
+	public static void update1(Node<Task> l,int c,boolean b) {
+		if(b!=true) {
+			l.getValue().subCredit(c);
+			if(l.getValue().getCredit()<10) {
+				l.getValue().addCredit(100);
+			}
+		}
+		else {
+			l.setNext(l.getNext());;
+		}
+	}
